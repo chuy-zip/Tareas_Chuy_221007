b7b42e4eb93a65f3b665cec1cc660381751160ca
chuy-zip <74995966+chuy-zip@users.noreply.github.com>
2022-09-21 09:08:04 -0600

    First commit Library exercise

---
+++ EjercicioEstudiante/src/main/NotaUI.java
@@ -0,0 +1,14 @@
+package main;
+
+public class NotaUI {
+	Asignatura asignatura_1;
+	Asignatura asignatura_2;
+	Asignatura asignatura_3;
+	Asignatura asignatura_4;
+	Asignatura asignatura_5;
+	
+	public static void main(String[] args) 
+	{
+		
+	}
+}
